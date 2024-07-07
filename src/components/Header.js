0a30cdbd96698daf379857604f0861880c540c0a
Nailtonssantos <nltsilva8@gmail.com>
2024-07-07 18:12:51 -0400

    Menu e Responsividade OK

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
-import logo from './path/logo.png'; // Altere o caminho para o logotipo do Pinterest
+import logo from './path/logo.png';
 
-const Header = () => {
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <div className="left">
-        <img src={logo} alt="Apey" className="logo" />
-        <nav>
+        <img src={logo} alt="Apey Logo" className="logo" />
+        <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
           <ul>
             <li>Hoje</li>
             <li>Explorar</li>
-          </ul>
-        </nav>
-      </div>
-      <div className="right">
-        <nav>
-          <ul>
             <li>Sobre</li>
             <li>Business</li>
             <li>Blog</li>
           </ul>
         </nav>
+      </div>
+      <div className="right">
         <div className="auth-buttons">
           <button className="login">Entrar</button>
           <button className="signup">Criar conta</button>
         </div>
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </div>
     </header>
   );
-};
+}
 
 export default Header;
